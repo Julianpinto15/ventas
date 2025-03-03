b20b6a9ea4b91ed96801a33b646b173d958f5354
BARBOSA191919 <sbarbosarivas@gmail.com>
2025-03-03 17:23:07 -0500

    Se agrega avances finalizados con bd

--- DB/ventas.sql
+++ DB/ventas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 27-02-2025 a las 00:07:23
+-- Tiempo de generación: 03-03-2025 a las 22:21:56
 -- Versión del servidor: 8.0.30
 -- Versión de PHP: 8.1.10
 
@@ -35,14 +35,6 @@ CREATE TABLE `autor` (
   `paisorigen` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Volcado de datos para la tabla `autor`
---
-
-INSERT INTO `autor` (`idAutor`, `codigo`, `nombre`, `biografia`, `paisorigen`) VALUES
-(7, '22332', 'gabriel garcia marquez', 'Nació Aracataca en Magdalena', 'Colombia');
-
-
 -- --------------------------------------------------------
 
 --
@@ -61,10 +53,8 @@ CREATE TABLE `caja` (
 --
 
 INSERT INTO `caja` (`caja_id`, `caja_numero`, `caja_nombre`, `caja_efectivo`) VALUES
-
-(1, 1, 'Caja #1', 260000.00),
-(2, 2, 'Caja #2', 580000.00);
-
+(1, 1, 'Caja admin', 500000.00),
+(2, 2, 'Caja cajero', 3000000.00);
 
 -- --------------------------------------------------------
 
@@ -77,14 +67,6 @@ CREATE TABLE `categoria` (
   `categoria_nombre` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_spanish2_ci NOT NULL,
   `categoria_ubicacion` varchar(150) CHARACTER SET utf8mb3 COLLATE utf8mb3_spanish2_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish2_ci;
-
-
---
--- Volcado de datos para la tabla `categoria`
---
-
-INSERT INTO `categoria` (`categoria_id`, `categoria_nombre`, `categoria_ubicacion`) VALUES
-(5, 'Realismo Mágico', 'Pasillo 3');
 
 -- --------------------------------------------------------
 
@@ -105,14 +87,6 @@ CREATE TABLE `cliente` (
   `cliente_email` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_spanish2_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish2_ci;
 
---
--- Volcado de datos para la tabla `cliente`
---
-
-INSERT INTO `cliente` (`cliente_id`, `cliente_tipo_documento`, `cliente_numero_documento`, `cliente_nombre`, `cliente_apellido`, `cliente_provincia`, `cliente_ciudad`, `cliente_direccion`, `cliente_telefono`, `cliente_email`) VALUES
-(1, 'Otro', 'N/A', 'Publico', 'General', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A'),
-(2, 'Cedula', '2312321132', 'santiago', 'barbosa', 'HUILA', 'neiva', 'calle 38 # 7 A 51 las granjas', '3217696864', 'sbarrivas@gmail.com');
-
 -- --------------------------------------------------------
 
 --
@@ -126,14 +100,6 @@ CREATE TABLE `editorial` (
   `informacioncontacto` text,
   `pais` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Volcado de datos para la tabla `editorial`
---
-
-INSERT INTO `editorial` (`idEditorial`, `codigo`, `nombre`, `informacioncontacto`, `pais`) VALUES
-(6, '223232', 'planeta', 'Bogota 21123123121', 'colombia');
-
 
 -- --------------------------------------------------------
 
@@ -154,10 +120,8 @@ CREATE TABLE `empresa` (
 -- Volcado de datos para la tabla `empresa`
 --
 
-
 INSERT INTO `empresa` (`empresa_id`, `empresa_nombre`, `empresa_telefono`, `empresa_email`, `empresa_direccion`, `empresa_foto`) VALUES
 (1, 'Allbooks', '3217696864', 'allboooks12@gmail.com', 'carrera 6 # 32-12', 'R7M1B9Q7Q9-83.png');
-
 
 -- --------------------------------------------------------
 
@@ -183,15 +147,6 @@ CREATE TABLE `producto` (
   `idEditorial` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish2_ci;
 
-
---
--- Volcado de datos para la tabla `producto`
---
-
-INSERT INTO `producto` (`producto_id`, `producto_codigo`, `producto_nombre`, `producto_stock_total`, `producto_tipo_unidad`, `producto_precio_compra`, `producto_precio_venta`, `producto_marca`, `producto_modelo`, `producto_estado`, `producto_foto`, `categoria_id`, `id_subcategoria`, `idAutor`, `idEditorial`) VALUES
-(15, '82923', 'Cien años de seriedad', 43, 'Caja', 32000.00, 40000.00, 'planeta', '21321351', 'Habilitado', '82923_83.jpg', 5, 9, 7, 6),
-(16, '37271', 'el coronel', 86, 'Unidad', 32000.00, 40000.00, 'PLANETA', '3828712', 'Habilitado', '', 5, 9, 7, 6);
-
 -- --------------------------------------------------------
 
 --
@@ -203,14 +158,6 @@ CREATE TABLE `subcategoria` (
   `nombre` varchar(100) NOT NULL,
   `categoria_id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Volcado de datos para la tabla `subcategoria`
---
-
-INSERT INTO `subcategoria` (`id_subcategoria`, `nombre`, `categoria_id`) VALUES
-(9, 'Realismo Ficcion', 5);
-
 
 -- --------------------------------------------------------
 
@@ -236,14 +183,9 @@ CREATE TABLE `usuario` (
 -- Volcado de datos para la tabla `usuario`
 --
 
-
 INSERT INTO `usuario` (`usuario_id`, `usuario_nombre`, `usuario_apellido`, `usuario_email`, `usuario_usuario`, `usuario_clave`, `usuario_cargo`, `usuario_foto`, `caja_id`, `usuario_creado`, `usuario_actualizado`) VALUES
-(1, 'Administrador', 'Principal', 'admin@admin.com', 'Administrador', '$2y$10$od1ZIIaCTLIYjxcTY6zHYugid/SDtxxf5Ev2goOkhxQ1HBQhUzYnW', 'Administrador', '', 1, '2025-02-26 00:23:06', '2025-02-26 00:23:06'),
-(2, 'santiago', 'barbosa', 'santiago12@gmail.com', 'santi12', '$2y$10$lwnoIw/65DfRTr13PukeT.zPj9DeWOFCnrbUEWcdstS01/Z79rTMq', 'Administrador', 'santiago_94.jpg', 2, '2025-02-26 00:23:06', '2025-02-26 00:23:06'),
-(3, 'julian', 'pinto', 'admin123@gmail.com', 'admin123', '$2y$10$TmUaqFABQ6s8jXIE61zIZe1RM4M6Pr4NErwwwB1zHNb0nCQ0GyMta', 'Cajero', '', 1, '2025-02-26 00:23:06', '2025-02-26 00:23:06'),
-(5, 'Usuario administrador', 'usuarioadministrador', 'user12@gmail.com', 'user12', '$2y$10$CIlB0xBsoHzoGrWpE0Ub1OGX6.4MOEPYpDSf3yAAVYhZoaMAX4.Lu', 'Administrador', 'Usuario_administrador_16.png', 1, '2025-02-26 23:59:26', '2025-02-26 23:59:26'),
-(7, 'user cajero', 'user cajero', 'userca1@gmail.com', 'userca1', '$2y$10$u2UG6ZGSEhNDVrm3qoCPj.UEkuCsKHOIX/D4/BHsqlWe3IgPLRiRm', 'Cajero', 'user_cajero_73.png', 2, '2025-02-27 00:05:52', '2025-02-27 00:05:52');
-
+(5, 'Usuario ', 'Admin', 'user12@gmail.com', 'admin123', '$2y$10$CIlB0xBsoHzoGrWpE0Ub1OGX6.4MOEPYpDSf3yAAVYhZoaMAX4.Lu', 'Administrador', '', 1, '2025-02-26 23:59:26', '2025-02-26 23:59:26'),
+(8, 'Usuario', 'Cajero', 'cajero123@gmail.com', 'cajero123', '$2y$10$cV78228vzGB1wbNugMs4w.Oo9clwVxhl29iPyuC7.A3m95P9JLxuK', 'Cajero', '', 2, '2025-03-03 22:19:01', '2025-03-03 22:19:01');
 
 -- --------------------------------------------------------
 
@@ -264,18 +206,6 @@ CREATE TABLE `venta` (
   `caja_id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish2_ci;
 
---
--- Volcado de datos para la tabla `venta`
---
-
-INSERT INTO `venta` (`venta_id`, `venta_codigo`, `venta_fecha`, `venta_hora`, `venta_total`, `venta_pagado`, `venta_cambio`, `usuario_id`, `cliente_id`, `caja_id`) VALUES
-(1, 'P4S0X9N2O3-1', '2025-02-26', '11:51 am', 80000.00, 100000.00, 20000.00, 2, 2, 1),
-(2, 'X1T8P8I8E2-2', '2025-02-26', '11:52 am', 80000.00, 100000.00, 20000.00, 3, 2, 1),
-(3, 'G5Z3W7K6E9-3', '2025-02-26', '11:53 am', 120000.00, 130000.00, 10000.00, 3, 2, 2),
-(4, 'S5X2Q1F0Q2-4', '2025-02-26', '06:00 pm', 40000.00, 50000.00, 10000.00, 2, 1, 2),
-(5, 'R9I6S1V6R3-5', '2025-02-26', '06:26 pm', 120000.00, 130000.00, 10000.00, 2, 1, 2);
-
-
 -- --------------------------------------------------------
 
 --
@@ -293,17 +223,6 @@ CREATE TABLE `venta_detalle` (
   `producto_id` int NOT NULL,
   `autor` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_spanish2_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish2_ci;
-
---
--- Volcado de datos para la tabla `venta_detalle`
---
-
-INSERT INTO `venta_detalle` (`venta_detalle_id`, `venta_detalle_cantidad`, `venta_detalle_precio_compra`, `venta_detalle_precio_venta`, `venta_detalle_total`, `venta_detalle_descripcion`, `venta_codigo`, `producto_id`, `autor`) VALUES
-(4, 2, 32000.00, 40000.00, 80000.00, 'Cien años de seriedad', 'P4S0X9N2O3-1', 15, 'gabriel garcia marquez'),
-(5, 2, 32000.00, 40000.00, 80000.00, 'Cien años de seriedad', 'X1T8P8I8E2-2', 15, 'gabriel garcia marquez'),
-(6, 3, 32000.00, 40000.00, 120000.00, 'Cien años de seriedad', 'G5Z3W7K6E9-3', 15, 'gabriel garcia marquez'),
-(7, 1, 32000.00, 40000.00, 40000.00, 'el coronel', 'S5X2Q1F0Q2-4', 16, 'gabriel garcia marquez'),
-(8, 3, 32000.00, 40000.00, 120000.00, 'el coronel', 'R9I6S1V6R3-5', 16, 'gabriel garcia marquez');
 
 --
 -- Índices para tablas volcadas
@@ -395,35 +314,31 @@ ALTER TABLE `venta_detalle`
 -- AUTO_INCREMENT de la tabla `autor`
 --
 ALTER TABLE `autor`
-
-  MODIFY `idAutor` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
+  MODIFY `idAutor` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `caja`
 --
 ALTER TABLE `caja`
-  MODIFY `caja_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `caja_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `categoria_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `categoria_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `cliente_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `cliente_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `editorial`
 --
 ALTER TABLE `editorial`
-
-  MODIFY `idEditorial` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
+  MODIFY `idEditorial` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `empresa`
@@ -435,41 +350,31 @@ ALTER TABLE `empresa`
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-
-  MODIFY `producto_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
-
+  MODIFY `producto_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT de la tabla `subcategoria`
 --
 ALTER TABLE `subcategoria`
-
-  MODIFY `id_subcategoria` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
+  MODIFY `id_subcategoria` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-
-  MODIFY `usuario_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
+  MODIFY `usuario_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `venta`
 --
 ALTER TABLE `venta`
-
-  MODIFY `venta_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
+  MODIFY `venta_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `venta_detalle`
 --
 ALTER TABLE `venta_detalle`
-
-  MODIFY `venta_detalle_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
+  MODIFY `venta_detalle_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Restricciones para tablas volcadas
